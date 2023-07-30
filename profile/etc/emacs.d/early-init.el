bb5ea4a60238dfc251746afa109ff90c191ad2c2
Giap Tran <txgvnn@gmail.com>
2023-07-30 08:53:25 +0700

    profile: update emacs.d/early-init.el

--- profile/etc/emacs.d/early-init.el
+++ profile/etc/emacs.d/early-init.el
@@ -1,3 +1,58 @@
+;;; early-init.el -*- lexical-binding: t; -*-
+
+;; Emacs 27.1 introduced early-init.el, which is run before init.el, before
+;; package and UI initialization happens, and before site files are loaded.
+
+;; A big contributor to startup times is garbage collection. We up the gc
+;; threshold to temporarily prevent it from running, then reset it later by
+;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
+;; packages are compiled ahead-of-time when they are installed and site files
+;; are compiled when gccemacs is installed.
+;; REVIEW Remove after a month
+(setq comp-deferred-compilation nil
+      native-comp-deferred-compilation nil)
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+
+;; Contrary to what many Emacs users have in their configs, you don't need more
+;; than this to make UTF-8 the default coding system:
+(set-language-environment "UTF-8")
+;; ...but the clipboard's on Windows could be in another encoding (likely
+;; utf-16), so let Emacs/the OS decide what to use there.
+(setq selection-coding-system 'utf-8) ; with sugar on top
+
+;; Reduce *Message* noise at startup. An empty scratch buffer (or the dashboard)
+;; is more than enough.
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message user-login-name
+      inhibit-default-init t
+      ;; Shave seconds off startup time by starting the scratch buffer in
+      ;; `fundamental-mode', rather than, say, `org-mode' or `text-mode', which
+      ;; pull in a ton of packages. `doom/open-scratch-buffer' provides a better
+      ;; scratch buffer anyway.
+      initial-major-mode 'fundamental-mode
+      initial-scratch-message nil)
+
+
+;; Don't ping things that look like domain names.
+(setq ffap-machine-p-known 'reject)
+
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(setq tool-bar-mode nil
+      menu-bar-mode nil)
+
 (setq package-directory-list '("~/.guix-profile/share/emacs/site-lisp"))
 (push 'ace-window package-activated-list)
 (push 'anzu package-activated-list)
@@ -15,9 +70,12 @@
 (push 'dumb-jump package-activated-list)
 (push 'embark package-activated-list)
 (push 'embark-consult package-activated-list)
+(push 'engine-mode package-activated-list)
 (push 'envrc package-activated-list)
 (push 'expand-region package-activated-list)
 (push 'forge package-activated-list)
+(push 'gcmh package-activated-list)
+(push 'gist package-activated-list)
 (push 'git-gutter package-activated-list)
 (push 'helpful package-activated-list)
 (push 'hl-todo package-activated-list)
@@ -28,8 +86,10 @@
 (push 'multiple-cursors package-activated-list)
 (push 'ob-compile package-activated-list)
 (push 'orderless package-activated-list)
+(push 'org-bullets package-activated-list)
 (push 'perspective package-activated-list)
 (push 'pinentry package-activated-list)
+(push 'project-tasks package-activated-list)
 (push 'rainbow-delimiters package-activated-list)
 (push 'rainbow-mode package-activated-list)
 (push 'rg package-activated-list)
@@ -41,5 +101,7 @@
 (push 'vertico package-activated-list)
 (push 'volatile-highlights package-activated-list)
 (push 'vundo package-activated-list)
+(push 'xclip package-activated-list)
+(push 'yaml-mode package-activated-list)
 (push 'yasnippet package-activated-list)
 (push 'ztree package-activated-list)
