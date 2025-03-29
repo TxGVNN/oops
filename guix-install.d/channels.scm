c6806003d61c41ff4d167bea46cab621b82eaf14
Giap Tran <txgvnn@disroot.org>
2025-03-29 15:31:12 +0700

    guix-install.d: Update channel

--- guix-install.d/channels.scm
+++ guix-install.d/channels.scm
@@ -1,10 +1,9 @@
 (list (channel
        (name 'guxti)
        (branch "master")
-       (commit "7f23eae574a23727c65bffec94a0fc9e5ebadf86")
+       (commit "39f9d673fd9a1525ec7633c3475472c25941aaf4")
        (url "https://github.com/TxGVNN/guxti"))
       (channel
        (name 'guix)
        (branch "master")
-       (commit "2c54c2db410ebdda8cd71716315e4ea4d31befbd")
        (url "https://git.savannah.gnu.org/git/guix.git")))
