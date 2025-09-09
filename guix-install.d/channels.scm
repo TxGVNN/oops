00e73cfef4bf1bd3c06d7fe7d1f34edb8440693b
Giap Tran <txgvnn@disroot.org>
2025-09-09 23:50:12 +0700

    guix-install.d/channels.scm: Add channel introductions.

--- guix-install.d/channels.scm
+++ guix-install.d/channels.scm
@@ -1,9 +1,20 @@
 (list (channel
        (name 'guxti)
        (branch "master")
-       (commit "faaec8068fd094c085a4b47c7458f2f498c78ca8")
-       (url "https://github.com/TxGVNN/guxti"))
+       (commit "f622b19bfeb38c3624f204bc045a0148f6565717")
+       (url "https://github.com/TxGVNN/guxti")
+       (introduction
+        (make-channel-introduction
+         "34ce27aeadde4ab8b26802453628f713155c0e96"
+         (openpgp-fingerprint
+          "97EE 7685 F353 F3BB AAFC  D3DA 8D6C 190A CBFE 117C"))))
+
       (channel
        (name 'guix)
        (branch "master")
-       (url "https://codeberg.org/guix/guix.git")))
+       (url "https://codeberg.org/guix/guix.git")
+       (introduction
+        (make-channel-introduction
+         "5f0d6215e4ae19fe33eaf931de242a64f81f29ee"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
