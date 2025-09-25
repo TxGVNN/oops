484d9c1b2cfe5bcdb382d2d56d86e49cfadad131
Giap Tran <txgvnn@disroot.org>
2025-09-25 21:14:36 +0700

    fix a bug

--- guix-install.d/channels.scm
+++ guix-install.d/channels.scm
@@ -15,6 +15,6 @@
        (url "https://codeberg.org/guix/guix.git")
        (introduction
         (make-channel-introduction
-         "5f0d6215e4ae19fe33eaf931de242a64f81f29ee"
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
           "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
